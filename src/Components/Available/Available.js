f7aa29f1d54cd2ac1869785b53800d203721fb52
Markosaur <mark3pace@gmail.com>
2019-10-02 09:32:23 -0600

    components set up

---
+++ src/Components/Available/Available.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Available extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/Components/MyGroups/MyGroups.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyGroups extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
